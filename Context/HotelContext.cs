ba95bf88606f344664deddfd0ebf667a922acb76
Molnár Balázs <molnarbalazs1226@gmail.com>
2024-11-07 14:30:02 +0100

    Add project files.

---
+++ Context/HotelContext.cs
@@ -0,0 +1,15 @@
+﻿using HotelBookingApi.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Numerics;
+
+namespace HotelBookingApi.Context
+{
+    public class HotelContext : DbContext
+    {
+        public HotelContext(DbContextOptions<HotelContext> options) : base(options)
+        { }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Guest> Guests { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+    }
+}
